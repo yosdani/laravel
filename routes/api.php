92db7a1d54ebbc3380cceaff1274654bf461e506
Daniel <dvg.sojo@gmail.com>
2021-05-20 19:30:49 -0400

    Logout end

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['jwt.auth','admin']], function(){
-    //Routes for roles
-    //Route for get all roles
-    Route::get('/roles', 'RoleController@getRoles' );
-    //Route for get role by id
-    Route::get('/roles/{id}', 'RoleController@getRoleById' );
-    //Route for create a new role
-    Route::post('/roles', 'RoleController@createRole' );
-    //Route for update a role
-    Route::put('/roles/{id}', 'RoleController@updateRole' );
-    //Route for delete a role
-    Route::delete('/roles/{id}', 'RoleController@deleteRole' );
-});
-
 //Routes for authenticate
 //Route for login
 Route::put('/auth', 'AuthUserController@login');
 //Route for register
 Route::post('/auth', 'AuthUserController@register');
+
+Route::group(['middleware' => ['jwt.auth','admin']], function(){
+
+    //Route for logout
+    Route::delete('/auth', 'AuthUserController@logout');
+
+    Route::group(['middleware' => ['admin']], function(){
+        //Routes for roles
+        //Route for get all roles
+        Route::get('/roles', 'RoleController@getRoles' );
+        //Route for get role by id
+        Route::get('/roles/{id}', 'RoleController@getRoleById' );
+        //Route for create a new role
+        Route::post('/roles', 'RoleController@createRole' );
+        //Route for update a role
+        Route::put('/roles/{id}', 'RoleController@updateRole' );
+        //Route for delete a role
+        Route::delete('/roles/{id}', 'RoleController@deleteRole' );
+    });
+});
