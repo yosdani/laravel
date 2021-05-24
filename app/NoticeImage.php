14b40e3f6f1ebbdf3c545ee842348b0b7ed953a2
tabares <ytabaressilverio86@gmail.com>
2021-05-24 06:43:55 -0700

    fix: Models for Notice and NoticeImage

---
+++ app/NoticeImage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NoticeImage extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'galery';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string
+     */
+    protected $fillable = [
+        'image',
+    ];
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @return  \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public  function notice()
+    {
+        return $this->belongsTo(\App\Notice::class, 'idNotice','id');
+    }
+}
+
