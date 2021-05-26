51ec765716917f9069bf87f5870e4923e0c4fa21
Daniel <dvg.sojo@gmail.com>
2021-05-26 13:01:28 -0400

    Mount the first vue component example

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,13 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component(
+    'example-component',
+    require('./components/ExampleComponent').default,
+    {
+        name: 'example-component'
+    }
+);
 
 const app = new Vue({
     el: '#app'
