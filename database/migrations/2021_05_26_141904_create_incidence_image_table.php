bba41151d3fe467279a44128a0c325116ec7f3b1
tabares <ytabaressilverio86@gmail.com>
2021-05-31 07:52:45 -0700

    fix: Migration for incidence Images

---
+++ database/migrations/2021_05_26_141904_create_incidence_image_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncidenceImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidence_image', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image')->nullable();
+            $table->integer('idIncidence')->unsigned();
+            $table->foreign('idIncidence')->references('id')->on('Incidence');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidence_image');
+    }
+}
