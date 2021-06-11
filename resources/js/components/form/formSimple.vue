b9f4061be9f69d2e5f7310dc48656000a77144e2
Daniel <dvg.sojo@gmail.com>
2021-06-11 11:04:06 -0400

    Simple form

---
+++ resources/js/components/form/formSimple.vue
@@ -0,0 +1,77 @@
+<template>
+    <b-form @submit="onSubmit" v-if="show">
+                <input type="hidden" name="_token" :value="csrf">
+
+                    <b-form-group id="input-group-2" :label="formOut.label" label-for="input-2">
+                        <b-form-input
+                        id="input-2"
+                        v-model="form.name"
+                        :placeholder="formOut.placeholder"
+                        :required="formOut.required"
+                        ></b-form-input>
+                    </b-form-group>
+
+                    <b-button type="submit" variant="primary">{{ formOut.action }}</b-button>
+                </b-form>
+</template>
+
+<script>
+export default {
+    props: ["formOut"],
+    data() {
+        return {
+            form: {},
+            show: true,
+            csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        };
+    },
+    mounted() {
+        this.form = this.formOut.form;
+        this.form._token = this.csrf;
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        fetch(window.origin+'/'+this.formOut.uri,{
+            method: this.formOut.method,
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(this.form)
+        })
+        .then(response => response.json())
+        .then(response=>{
+            this.$swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Se ha adicionado correctamente',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            this.onReset(event);
+        })
+        .catch(err =>{
+            this.$swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong!',
+            })
+        })
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form = this.formOut;
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
