478db68d795d89fc23154a925199143a58e94340
tabares <ytabaressilverio86@gmail.com>
2021-06-11 11:45:38 -0700

    fix:Migrations for the News Images

---
+++ database/migrations/2021_06_11_184229_create_news_image_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news_image', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image')->nullable();
+            $table->integer('news_id')->unsigned();
+            $table->foreign('news_id')->references('id')->on('news');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news_image');
+    }
+}
