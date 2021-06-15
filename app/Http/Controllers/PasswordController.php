5e7e1ed0030b6d15f109788ba28f4f6957fb0f50
Daniel <dvg.sojo@gmail.com>
2021-06-15 07:57:19 -0400

    Document again the auth

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -15,7 +15,39 @@ class PasswordController extends Controller
      * Forgot Password
      * @param Request $request
      * @return JsonResponse
+     *  @OA\Post (
+     *      path="/password",
+     *      tags={"Auth"},
+     *      summary="Forgot password",
+     *      description="The user reset the password and send by email",
+     *     @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *             @OA\Schema(
+     *                 type="object",
+     *                  @OA\Property(
+     *                     property="email",
+     *                     description="Email of user",
+     *                     type="string"
+     *                 ),
+     *             )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
      *
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="The email not exists"
+     *      ),
+     *       @OA\Response(
+     *          response=404,
+     *          description="You are not provided a email"
+     *      )
+     * )
      */
     public function forgotPassword(Request $request): JsonResponse
     {
@@ -24,6 +56,13 @@ class PasswordController extends Controller
                 'success' => false,
                 'message' => 'You have not provided your email address...'
             ], 404);
+        }
+
+        if (!User::where('email', '=', $request->email)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'The email not exists...'
+            ], 400);
         }
 
         $email = $request->email;
