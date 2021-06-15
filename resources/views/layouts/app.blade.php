ec4074a0316801632f830620b0271ad795026ddc
renegl <Brady262324>
2021-06-15 18:31:15 -0400

    setting favicon an meta title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Participacion Ciudadana') }}</title>
+    <title>Participación Ciudadana Panel</title>
 
     <!-- Scripts -->
     <script>
@@ -23,7 +23,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon.png">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
