ca68d17363dce067ff1bf1706a567b1e318e12d7
renegl <Brady262324>
2021-06-21 13:47:28 -0400

    User filters field to string

--- database/migrations/2021_06_19_180948_add_filters_user_table.php
+++ database/migrations/2021_06_19_180948_add_filters_user_table.php
@@ -14,7 +14,7 @@ class AddFiltersUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('filters');
+            $table->longText('filters')->nullable();
 
             //
         });
