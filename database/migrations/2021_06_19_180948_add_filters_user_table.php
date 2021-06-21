2783cb790102209bbde1110d1108f9e8d7dcec30
renegl <Brady262324>
2021-06-21 13:30:05 -0400

    Refactoring filters migrations

--- database/migrations/2021_06_19_180948_add_filters_user_table.php
+++ database/migrations/2021_06_19_180948_add_filters_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFitersUserTable extends Migration
+class AddFiltersUserTable extends Migration
 {
     /**
      * Run the migrations.
